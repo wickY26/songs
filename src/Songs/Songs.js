131b858b0e95c7e657e4c3053181bdfb736fdd3c
Poyraz <poyrazylmaz@gmail.com>
2019-03-30 08:13:41 +0300

    Songs component is created...

---
+++ src/Songs/Songs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Songs.module.css';
+
+const Songs = () => {
+  return (
+    <div className={classes.wrapper}>
+      
+    </div>
+  )
+}
+
+export default Songs;
