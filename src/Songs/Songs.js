59adc788903155e2d22f7707d23ade6da45ce461
Poyraz <poyrazylmaz@gmail.com>
2019-03-30 08:53:12 +0300

    fetch songs data from json and set state of component and render basic content...

--- src/Songs/Songs.js
+++ src/Songs/Songs.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import classes from './Songs.module.css';
 
 const Songs = () => {
+  const [songs, setSongs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setError(false);
+        setLoading(true);
+        const result = await axios('/songs.json');
+        setLoading(false);
+        setSongs([...result.data]);
+      } catch (err) {
+        setLoading(false);
+        setError(true);
+      }
+    })();
+  }, []);
+
+  const content = () => {
+    if (error) {
+      return <div>An error is occured</div>;
+    } else if (loading) {
+      return <div>Loading...</div>;
+    } else {
+      return songs.map(song => <div>{song.title}</div>);
+    }
+  }
+
   return (
     <div className={classes.wrapper}>
-      
+      {content()}
     </div>
   )
 }
