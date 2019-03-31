f94bf3c8308422dcc5ae8df15044530f13475f9a
Poyraz <poyrazylmaz@gmail.com>
2019-03-31 20:25:10 +0300

    style changes...

--- src/Songs/SongsFilter/SongsFilter.js
+++ src/Songs/SongsFilter/SongsFilter.js
@@ -3,6 +3,7 @@ import classes from './SongsFilter.module.css';
 
 const SongsFilter = ({ onFilterChange }) => {
   const [filter, setFilter] = useState({ searchKey: '', level: 0 });
+  const levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   useEffect(() => {
     onFilterChange(filter);
@@ -17,11 +18,21 @@ const SongsFilter = ({ onFilterChange }) => {
   }
 
   return (
-    <div>
-      <input type="text" value={filter.searchKey} onChange={handleInputChange} />
-      <select value={filter.level} onChange={handleSelectChange}>
-        <option></option>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((level) => <option value={level}>{level}</option>)}
+    <div className={classes.wrapper}>
+      <input
+        type="text"
+        placeholder="Search Songs"
+        className={classes.search}
+        value={filter.searchKey}
+        onChange={handleInputChange}
+      />
+      <select
+        className={classes.level}
+        value={filter.level}
+        onChange={handleSelectChange}
+      >
+        <option>Select Level</option>
+        {levels.map((level) => <option value={level}>{level}</option>)}
       </select>
     </div>
   )
