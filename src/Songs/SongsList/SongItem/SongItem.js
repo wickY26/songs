990422bb5198a4d64d3586bb6185bd8ba5704c4e
Poyraz <poyrazylmaz@gmail.com>
2019-03-31 16:11:23 +0300

    level business is lifted to SongLevel component...

--- src/Songs/SongsList/SongItem/SongItem.js
+++ src/Songs/SongsList/SongItem/SongItem.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import Ratings from 'react-ratings-declarative';
 import classes from './SongItem.module.css';
+import SongLevel from './SongLevel/SongLevel';
 
 const SongItem = ({ song, style, onRatingChange }) => {
   const { artist, level, rating, title } = song;
-  const radius = 175;
-  const diameter = Math.round(Math.PI * radius * 2);
-  const getOffset = (val = 0) => Math.round((15 - Math.min(val, 15)) / 15 * diameter);
 
   return (
     <div style={style} className={classes.wrapper}>
       <div className={classes.logo} />
-      <div className={classes.level}>
-        <svg width="64" height="64" viewBox="-25 -25 400 400">
-          <circle stroke="#212326" cx="175" cy="175" r="175" strokeWidth="24" fill="none" />
-          <circle stroke="#4fc514" transform="rotate(-90 175 175)" cx="175" cy="175" r="175" strokeDasharray="1100" strokeWidth="24" strokeDashoffset="1100" strokeLinecap="round" fill="none" style={{ strokeDashoffset: getOffset(level) }} />
-          {level}
-          <text style={{ fontSize: '8rem' }} fill={'#fff'} x={radius} y={radius} textAnchor="middle" dominantBaseline="central">
-            {level}
-          </text>
-        </svg>
-      </div>
+      <SongLevel level={level} />
       <div className={classes.content}>
         <div className={classes.title}>{title}</div>
         <div className={classes.info}>
